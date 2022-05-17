74bf52af5a07da35861a60cda8ca77db3a805e66
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-05-17 20:59:37 +0100

    my attacking contract

--- 3-CoinFlip/Attack.sol
+++ 3-CoinFlip/Attack.sol
@@ -12,8 +12,7 @@ contract Attack {
 
     CoinFlip public victim;
 
-    uint256 FACTOR =
-        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _victimAddress) public {
         victim = CoinFlip(_victimAddress);
